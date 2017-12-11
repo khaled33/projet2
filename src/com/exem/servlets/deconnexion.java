597d2929649a855109637ff2e02cd4bc7a22c822
khaled <khaledhizawi@gmail.com>
2017-12-11 22:17:14 +0100

    cc

---
+++ src/com/exem/servlets/deconnexion.java
@@ -0,0 +1,24 @@
+package com.exem.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class deconnexion extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+    
+    
+    public deconnexion() {
+        super();
+        }
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String msg="salut user";
+		request.setAttribute("msg", msg);
+		this.getServletContext().getRequestDispatcher("/WEB-INF/connexion.jsp").forward( request, response );
+	}
+
+}
